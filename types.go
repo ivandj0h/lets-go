c9685848c344dc0a4c74bed48263399e9436def1
ivandjoh <ivandi.djoh@google.com>
2024-06-05 12:31:52 +0800

    updating task

---
+++ types.go
@@ -0,0 +1,12 @@
+package main
+
+import "time"
+
+type Task struct {
+	ID           int64     `json:"id"`
+	Name         string    `json:"name"`
+	Status       string    `json:"status"`
+	ProjectID    int64     `json:"projectID"`
+	AssignedToID int64     `json:"assignedTo"`
+	CreatedAt    time.Time `json:"createdAt"`
+}
---
+++ utils.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	err := json.NewEncoder(w).Encode(v)
+	if err != nil {
+		return
+	}
+}
+
+func GetTokenFromRequest(r *http.Request) string {
+	tokenAuth := r.Header.Get("Authorization")
+	tokenQuery := r.URL.Query().Get("token")
+
+	if tokenAuth != "" {
+		return tokenAuth
+	}
+
+	if tokenQuery != "" {
+		return tokenQuery
+	}
+
+	return ""
+}
